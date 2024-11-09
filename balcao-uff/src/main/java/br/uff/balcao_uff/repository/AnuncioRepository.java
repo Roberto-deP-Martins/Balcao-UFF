5ee00747f889f594af6bc8f89d166efde57f8394
jhonatan win casa <jhonatansilva@id.uff.br>
2024-11-09 14:26:15 -0300

    chore(repository): Att import nos repositories

--- balcao-uff/src/main/java/br/uff/balcao_uff/repository/AnuncioRepository.java
+++ balcao-uff/src/main/java/br/uff/balcao_uff/repository/AnuncioRepository.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import br.uff.balcao_uff.entity.AnuncioEntity;
 import br.uff.balcao_uff.entity.MessageEntity;
 
-public interface AnuncioRepository extends JpaRepository<MessageEntity, Long> {
+public interface AnuncioRepository extends JpaRepository<AnuncioEntity, Long> {
 
 	List<MessageEntity> findByReceiverId(Long receiverId);
 }
--- balcao-uff/src/main/java/br/uff/balcao_uff/repository/MessageRepository.java
+++ balcao-uff/src/main/java/br/uff/balcao_uff/repository/MessageRepository.java
@@ -2,9 +2,10 @@ package br.uff.balcao_uff.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import br.uff.balcao_uff.entity.MessageEntity;
 import br.uff.balcao_uff.entity.UserEntity;
 
-public interface MessageRepository extends JpaRepository<UserEntity, Long> {
-	
+public interface MessageRepository extends JpaRepository<MessageEntity, Long> {
+
 	UserEntity findByEmail(String email);
 }
