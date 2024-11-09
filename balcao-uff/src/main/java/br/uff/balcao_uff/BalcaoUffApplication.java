09dc34d4906a7fce5cb5e0e50186720874b2524d
jhonatan win casa <jhonatansilva@id.uff.br>
2024-11-09 01:53:49 -0300

    Add projeto spring

---
+++ balcao-uff/src/main/java/br/uff/balcao_uff/BalcaoUffApplication.java
@@ -0,0 +1,13 @@
+package br.uff.balcao_uff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalcaoUffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalcaoUffApplication.class, args);
+	}
+
+}
---
+++ balcao-uff/src/test/java/br/uff/balcao_uff/BalcaoUffApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uff.balcao_uff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalcaoUffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
