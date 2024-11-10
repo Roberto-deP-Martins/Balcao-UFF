88288cf1eec4231e54885569c8dc5bde574e575f
jhonatan-note-ubuntu <jhonatan.oliveira.dev@gmail.com>
2024-11-10 07:33:32 -0300

    feat(database): criar tabela tb_message

---
+++ balcao-uff/src/main/resources/db/migration/V3__create_message_table.sql
@@ -0,0 +1,12 @@
+-- V3__create_message_table.sql
+CREATE TABLE IF NOT EXISTS tb_message (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    content TEXT NOT NULL,
+    isRead BOOLEAN NOT NULL,
+    sender_id BIGINT,
+    receiver_id BIGINT,
+    anuncio_id BIGINT,
+    CONSTRAINT fk_sender FOREIGN KEY (sender_id) REFERENCES tb_user(id),
+    CONSTRAINT fk_receiver FOREIGN KEY (receiver_id) REFERENCES tb_user(id),
+    CONSTRAINT fk_anuncio FOREIGN KEY (anuncio_id) REFERENCES tb_anuncio(id)
+);
