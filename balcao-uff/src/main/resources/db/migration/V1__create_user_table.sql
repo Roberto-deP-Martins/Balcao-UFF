7dcc8b3eee7c2f47c6cc915dad03dda93014ce87
jhonatan-note-ubuntu <jhonatan.oliveira.dev@gmail.com>
2024-11-10 07:32:53 -0300

    feat(database): criar tabela tb_user

---
+++ balcao-uff/src/main/resources/db/migration/V1__create_user_table.sql
@@ -0,0 +1,9 @@
+-- V1__create_user_table.sql
+CREATE TABLE IF NOT EXISTS tb_user (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    cpf VARCHAR(14) NOT NULL,
+    role VARCHAR(255) NOT NULL
+);
