3f520e04998892b6039962d5a367cbb79982944d
jhonatan win casa <jhonatansilva@id.uff.br>
2024-11-14 13:32:57 -0300

    chore: security att

--- balcao-uff/src/main/java/br/uff/balcao_uff/configuration/security/SecurityConfigurations.java
+++ balcao-uff/src/main/java/br/uff/balcao_uff/configuration/security/SecurityConfigurations.java
@@ -52,17 +52,4 @@ public class SecurityConfigurations {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
-            }
-        };
-    }
 }
