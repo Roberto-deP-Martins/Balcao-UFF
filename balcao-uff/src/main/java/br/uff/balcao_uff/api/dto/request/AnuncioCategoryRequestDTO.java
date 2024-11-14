1ec4fe9825c33992fb8e6a64946848a970f7b392
jhonatan win casa <jhonatansilva@id.uff.br>
2024-11-14 16:13:52 -0300

    feat(dto): add DTOs busca categoria e delete

---
+++ balcao-uff/src/main/java/br/uff/balcao_uff/api/dto/request/AnuncioCategoryRequestDTO.java
@@ -0,0 +1,4 @@
+package br.uff.balcao_uff.api.dto.request;
+
+public record AnuncioCategoryRequestDTO(String category) {
+}
---
+++ balcao-uff/src/main/java/br/uff/balcao_uff/api/dto/request/AnuncioDeleteRequestDTO.java
@@ -0,0 +1,5 @@
+package br.uff.balcao_uff.api.dto.request;
+
+public record AnuncioDeleteRequestDTO(Long id) {
+
+}
